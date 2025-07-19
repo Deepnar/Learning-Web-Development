4a40b9ed21f86f7cd2b543d866845c26a258837d
Deepesh Sonar <18deepnar@gmail.com>
2025-07-19 20:35:25 +0530

    started with learning js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log("Hello from our 1st JS File", 3 * 4, 3 ** 4)
+let age = Math.floor(Math.random() * 100);
+console.log(age)
+if (age >= 18) {
+    console.log(`u can enter u are ${age}`)
+}
+else {
+    console.log("U cannot enter u are under the age  18")
+}
